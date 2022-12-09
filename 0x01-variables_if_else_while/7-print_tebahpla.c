d178ba9a9526614f3c809383123638039ce49fcf
Igwe Miracle <miracleigwe48@gmail.com>
2022-12-09 08:27:34 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch, new_line;
+
+new_line = '\n';
+for (ch = 'z';ch >= 'a';)
+
+{
+putchar(ch);
+ch--;
+}
+putchar(new_line);
+return (0);
+}
