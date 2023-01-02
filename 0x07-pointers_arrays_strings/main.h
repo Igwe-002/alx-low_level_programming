9bf051b56b0246b4c58bb75224928ab32678de69
Igwe Miracle <miracleigwe48@gmail.com>
2023-01-02 09:27:15 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /**
- * File: holberton.h
+ * File: Main.h
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-pointers_arrays_strings directory.
  */
