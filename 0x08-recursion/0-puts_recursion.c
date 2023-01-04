badeecb99fdaf472094d9c7e17fdbc39c7407b96
Igwe Miracle <miracleigwe48@gmail.com>
2023-01-04 03:45:17 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,20 @@
 #include "main.h"
+
 /**
- * _puts_recursion - Prints a string.
- * @s: The string to be printed.
+ * _puts_recursion - puts, but recursively
+ *
+ * @s: string to puts
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == 0)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
 }
