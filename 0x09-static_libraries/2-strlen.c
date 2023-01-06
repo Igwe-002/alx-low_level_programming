afc2fe3d0639f29c2e3185ccda2d70345d0ccee7
Igwe Miracle <miracleigwe48@gmail.com>
2023-01-06 11:50:03 -0800

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: length.
+ */
+int _strlen(char *s)
+{
+		int i;
+
+			for (i = 0; s[i] != '\0'; i++)
+					{
+							}
+
+				return (i);
+}
