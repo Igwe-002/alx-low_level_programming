b694e475e80fd36fc2510b372c62ba8fcdd1237e
Igwe Miracle <miracleigwe48@gmail.com>
2023-01-17 10:58:28 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,10 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
 
 /*
  * File: 3-function_like_macro.h
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef abs_h
+#define abs_h
+#define ABS(x) (x > 0 ? x : x * -1)
 #endif
