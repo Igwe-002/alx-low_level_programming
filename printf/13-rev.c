d31be523faa240728b4d4ddeb00a1d3743133fe8
Igwe Miracle <miracleigwe48@gmail.com>
2023-01-22 12:20:16 -0800

    13-rev.c

---
+++ printf/13-rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_fmt - Handles conversion specifier "r"
+ * @buffer: Pointer to struct buffer
+ * @args: the string to be printed in reverse
+ *
+ * Return: Pointer to struct buffer.
+ */
+mk_buffer rev_fmt(mk_buffer buffer, va_list args)
+{
+	char *str;
+	int len;
+
+	str = va_arg(args, char *);
+	len = _strlen(str);
+	len--;
+	while (len >= 0)
+	{
+		*buffer.box = str[len];
+		buffer.box++;
+		buffer.size++;
+		len--;
+	}
+
+	buffer.box--;
+	return (buffer);
+}
